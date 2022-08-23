7655f1f966bcb0ac510d86274cce75ad83c7a3fc
afifhibatullahh <afifhibatullah59@gmail.com>
2022-08-23 16:17:18 +0700

    add item,rab,paket model

---
+++ app/Models/Rab.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rab extends Model
+{
+    use HasFactory;
+    protected $table = 'rabs';
+    protected $guarded = [
+        'id'
+    ];
+}
