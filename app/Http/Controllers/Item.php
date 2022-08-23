35545f9058be065e72a1890c34e86b8cf04273c4
afifhibatullahh <afifhibatullah59@gmail.com>
2022-08-23 16:17:33 +0700

    updatte crud Item

--- app/Http/Controllers/Item.php
+++ app/Http/Controllers/Item.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item as ModelsItem;
 use Illuminate\Http\Request;
 
 class Item extends Controller
 {
     public function index()
     {
-        return view('item.index');
+        $item = ModelsItem::all();
+        return view('item.index', compact(['item']));
     }
 
     public function add()
@@ -17,10 +19,43 @@ class Item extends Controller
     }
     public function edit($id = null)
     {
-        return view('item.edit',  ['id' => $id]);
+        if ($id !== null) {
+            $item = ModelsItem::where('id', $id)->first();
+            return view('item.edit',  compact(['item']));
+        }
+        return redirect('/barang');
     }
     public function show($id = null)
     {
-        return view('item.detail', ['id' => $id]);
+        $item = ModelsItem::where('id', $id)->first();
+        return view('item.detail', compact(['item']));
+    }
+
+    public function store(Request $request, $id = null)
+    {
+        $data = [
+            'nama_barang' => $request->nama,
+            'spesifikasi' => $request->spesifikasi,
+            'satuan' => $request->satuan,
+            'harga' => $request->harga,
+            'sumber' => $request->sumber,
+            'gambar' => $request->gambar,
+            'jenis' => $request->jenis,
+        ];
+        if ($id == null) {
+            ModelsItem::create($data);
+
+            return redirect('/barang');
+        } else {
+            $item = ModelsItem::find($id);
+            $item->update($data);
+            return redirect('/barang');
+        }
+    }
+
+    public function delete($id)
+    {
+        ModelsItem::destroy($id);
+        return redirect('/barang');
     }
 }
