7a02be184512324632abee7c2ff5eb457c09cf4d
afifhibatullahh <afifhibatullah59@gmail.com>
2023-01-03 23:46:59 +0700

    update dashboard

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Home extends Controller
 {
     public function index()
     {
-        return view('home');
+        $total_rab = DB::table('rabs')->whereYear('waktu_pelaksanaan', date('Y'))->get()->toArray();
+        $total_rab_accepted = DB::table('rabs')->whereYear('waktu_pelaksanaan', date('Y'))->where('status', '=', 'accepted')->get()->toArray();
+
+        $total_rab = \count($total_rab);
+        $total_rab_accepted = \count($total_rab_accepted);
+
+        $laboratorium_anggaran = DB::table('anggaran')
+            ->join('laboratorium', 'laboratorium.id', '=', 'anggaran.laboratorium')
+            ->whereYear('periode', \date('Y'))
+            ->select(['laboratorium.laboratorium', DB::raw('SUM(anggaran) as anggaran')])
+            ->groupBy('laboratorium')
+            ->get()->toArray();
+
+        return view('home', \compact(['total_rab', 'total_rab_accepted', 'laboratorium_anggaran']));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,80 @@
             <div class="col-12">
                 <div class="row align-items-center mb-2">
                     <div class="col">
-                        <h2 class="h5 page-title">Welcome!</h2>
+                        <h2 class="h5 page-title">Welcome <span style="font-size: 16px">
+                                {{Auth::user()->name}}
+                            </span>
+                            !
+                        </h2>
                     </div>
                 </div>
-
             </div>
+            <div class="col-md-6 col-xl-3 mb-4">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-3 text-center">
+                                <span class="circle circle-sm bg-primary">
+                                    <i class="fe fe-16 fe-shopping-bag text-white mb-0"></i>
+                                </span>
+                            </div>
+                            <div class="col">
+                                <p class="small text-muted mb-0" style="font-size: 9px">Total Rencana Anggaran
+                                    Belanja Tahun ini</p>
+                                <span class="h3 mb-0">{{$total_rab}}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-3 mb-4">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-3 text-center">
+                                <span class="circle circle-sm bg-primary">
+                                    <i class="fe fe-16 fe-check text-white mb-0"></i>
+                                </span>
+                            </div>
+                            <div class="col">
+                                <p class="small text-muted mb-0" style="font-size: 9px">Total Rencana Anggaran
+                                    Belanja yang disetujui Tahun ini</p>
+                                <span class="h3 mb-0">{{$total_rab_accepted}}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <strong class="card-title">Laboratorium</strong>
+                            <strong class="card-title">Anggaran (Rp)
+                                <p class="small text-muted mb-0" style="font-size: 9px">Tahun ini</p>
+                            </strong>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="list-group list-group-flush my-n3">
+                            <div class="list-group-item">
+                                @foreach ($laboratorium_anggaran as $item)
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <strong>{{$item->laboratorium}}</strong>
+                                    </div>
+                                    <div class="col-auto">
+                                        <strong>@convert($item->anggaran)</strong>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div> <!-- / .list-group -->
+                    </div> <!-- / .card-body -->
+                </div> <!-- / .card -->
+            </div> <!-- / .col-md-3 -->
         </div>
     </div>
 </main>
